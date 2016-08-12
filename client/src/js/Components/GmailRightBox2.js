2b7e15001be8c419cd9384036e7ac23f022bf56e
AnchanLavina <lavina.anchan.783@gmail.com>
2016-08-12 18:51:47 +0530

    express initial

---
+++ client/src/js/Components/GmailRightBox2.js
@@ -0,0 +1,37 @@
+var React=require("react");
+var Reply=require("./Reply.js");
+var GmailRightBox2= React.createClass({
+  getInitialState:function()
+  {
+  return({showModal:false});
+  }
+,
+  handleShowModal: function(e)
+  {
+    this.setState({showModal:true});
+  },
+  handleHideModal:function()
+  {
+    this.setState({showModal:false});
+  },
+  render: function() {
+var from2=this.props.from1;
+var subject2=this.props.subject;
+var date2=this.props.date;
+var id2=this.props.id;
+return (
+    <tr>
+      <td>{this.props.from1}</td>
+      <td>
+          <a href="#replyModal" onMouseDown={this.handleShowModal} data-toggle="modal">
+            {this.props.subject}
+          </a>
+          {this.state.showModal ? <Reply handleHideModal={this.handleHideModal} from2={from2} subject2={subject2} id2={id2}/> : null}
+      </td>
+      <td>{this.props.date}
+      </td>
+    </tr>
+     );
+  }
+});
+module.exports=GmailRightBox2;
